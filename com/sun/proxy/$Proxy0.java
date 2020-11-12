57ecc5df4221b4704e87ea102d8c5bb8e1318c93
yaocs2 <yaocs2@midea.com>
2020-11-12 15:17:34 +0800

    my-design-patterns-> JDK动态代理

---
+++ com/sun/proxy/$Proxy0.java
@@ -0,0 +1,99 @@
+// Decompiled by Jad v1.5.8g. Copyright 2001 Pavel Kouznetsov.
+// Jad home page: http://www.kpdus.com/jad.html
+// Decompiler options: packimports(3) 
+
+package com.sun.proxy;
+
+import com.jsonyao.cs.proxyPattern.UserService;
+import java.lang.reflect.*;
+
+public final class $Proxy0 extends Proxy
+    implements UserService
+{
+
+    public $Proxy0(InvocationHandler invocationhandler)
+    {
+        super(invocationhandler);
+    }
+
+    public final void save()
+    {
+        try
+        {
+            super.h.invoke(this, m3, null);
+            return;
+        }
+        catch(Error _ex) { }
+        catch(Throwable throwable)
+        {
+            throw new UndeclaredThrowableException(throwable);
+        }
+    }
+
+    public final boolean equals(Object obj)
+    {
+        try
+        {
+            return ((Boolean)super.h.invoke(this, m1, new Object[] {
+                obj
+            })).booleanValue();
+        }
+        catch(Error _ex) { }
+        catch(Throwable throwable)
+        {
+            throw new UndeclaredThrowableException(throwable);
+        }
+    }
+
+    public final int hashCode()
+    {
+        try
+        {
+            return ((Integer)super.h.invoke(this, m0, null)).intValue();
+        }
+        catch(Error _ex) { }
+        catch(Throwable throwable)
+        {
+            throw new UndeclaredThrowableException(throwable);
+        }
+    }
+
+    public final String toString()
+    {
+        try
+        {
+            return (String)super.h.invoke(this, m2, null);
+        }
+        catch(Error _ex) { }
+        catch(Throwable throwable)
+        {
+            throw new UndeclaredThrowableException(throwable);
+        }
+    }
+
+    private static Method m3;
+    private static Method m1;
+    private static Method m0;
+    private static Method m2;
+
+    static 
+    {
+        try
+        {
+            m3 = Class.forName("com.jsonyao.cs.proxyPattern.UserService").getMethod("save", new Class[0]);
+            m1 = Class.forName("java.lang.Object").getMethod("equals", new Class[] {
+                Class.forName("java.lang.Object")
+            });
+            m0 = Class.forName("java.lang.Object").getMethod("hashCode", new Class[0]);
+            m2 = Class.forName("java.lang.Object").getMethod("toString", new Class[0]);
+        }
+        catch(NoSuchMethodException nosuchmethodexception)
+        {
+            throw new NoSuchMethodError(nosuchmethodexception.getMessage());
+        }
+        catch(ClassNotFoundException classnotfoundexception)
+        {
+            throw new NoClassDefFoundError(classnotfoundexception.getMessage());
+        }
+    }
+}
